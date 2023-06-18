5f30d027731db9037ae3473bd658d84d0f245e5b
Justinus Andreas <justinus.andreas@dansmultipro.com>
2023-06-18 18:02:31 +0700

    server-side caching implemented

--- src/services/postgres/CollaborationsService.js
+++ src/services/postgres/CollaborationsService.js
@@ -3,8 +3,9 @@ const { Pool } = require('pg');
 const InvariantError = require('../../exceptions/InvariantError');
 
 class CollaborationsService {
-  constructor() {
+  constructor(cacheService) {
     this.pool = new Pool();
+    this.cacheService = cacheService;
   }
 
   async addCollaboration(noteId, userId) {
@@ -18,6 +19,8 @@ class CollaborationsService {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal ditambahkan');
     }
+
+    await this.cacheService.delete(`notes:${userId}`);
     return result.rows[0].id;
   }
 
@@ -32,6 +35,8 @@ class CollaborationsService {
     if (!result.rows.length) {
       throw new InvariantError('Kolaborasi gagal dihapus');
     }
+
+    await this.cacheService.delete(`notes:${userId}`);
   }
 
   async verifyCollaborator(noteId, userId) {
